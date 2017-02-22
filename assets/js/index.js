076f2712fc6bf70d5de7f1d388e0a665f0f36eda
paul.guillamon@gmail.com <darksector>
2017-02-22 04:27:14 +0100

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,14 @@
 function toggleNav() {
   var x = document.getElementById('videoNav');
   var y = document.getElementById('btnNav');
+
     if (x.style.width === '250px') {
         x.style.width = '0px';
         y.style.left = '0px';
+        y.innerHTML='&#9776;';
     } else {
         x.style.width = '250px';
         y.style.left = '250px';
+        y.innerHTML='&#9932;';
     }
 }
